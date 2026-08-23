f5b5f8ec68a2b5bc7a35a12adf5d2fd5ecab50e6
Anna <anyapavelko24@gmail.com>
2026-08-23 19:56:54 +0300

    Configure pino pretty logging

--- src/server.js
+++ src/server.js
@@ -7,7 +7,13 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(pinoHttp());
+app.use(
+  pinoHttp({
+    transport: {
+      target: 'pino-pretty',
+    },
+  }),
+);
 app.use(cors());
 app.use(express.json());
 
